acc6fca728c57cc68e47d77c8fcab6114f5fbf4c
Aaron Ackerman <theron17@gmail.com>
2015-04-21 21:20:13 -0500

    Added webpack preprocessor for karma

--- karma.conf.js
+++ karma.conf.js
@@ -12,7 +12,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      '__tests__/**/*_test.js'
+      '__tests__/*_test.js'
     ],
 
 
@@ -24,6 +24,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      '__tests__/*_test.js': ['webpack']
     },
 
 
